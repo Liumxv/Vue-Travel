b3a58a9a035fb6f78dfa0ce6cbbf29d291e59459
Liumx <liumxv@126.com>
2019-10-09 15:22:12 +0800

    add search to city

--- src/pages/city/components/Header.vue
+++ src/pages/city/components/Header.vue
@@ -17,20 +17,19 @@ export default {
 @import '~styles/varibles.styl';
 
 .header
-  // position relative
+  position relative
   height .86rem
   line-height .86rem
-  // overflow hidden
+  overflow hidden
   color #fff
   text-align center
   background $bgColor
   .header-back
-    // position absolute
-    // top 0
-    // left 0
+    position absolute
+    top 0
+    left 0
     width .64rem,
     font-size .4rem
     padding-left .2rem
     color #fff
-    background red
 </style>
