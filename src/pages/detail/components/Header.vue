4fcfb036461f5417388ab9b85693d8741816057c
Liumx <liumxv@126.com>
2019-10-10 16:19:33 +0800

    header animation

---
+++ src/pages/detail/components/Header.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <div class="header-abs" v-show="showAbs">
+      <div class="iconfont header-abs-back" @click="handleBackClick()">&#xe624;</div>
+    </div>
+    <div class="header-fixed" v-show="!showAbs" :style="opacityStyle">
+      <div class="iconfont header-fixed-back" @click="handleBackClick()">&#xe624;</div>
+      <div>景点详情</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailHeader',
+  data () {
+    return {
+      showAbs: true,
+      opacityStyle: {
+        opacity: 0
+      }
+    }
+  },
+  methods: {
+    handleBackClick () {
+      this.$router.go(-1)
+    },
+    handleScroll (e) {
+      const top = document.documentElement.scrollTop
+      if (top > 60 && top < 140) {
+        const opacity = top / 140
+        this.opacityStyle = {
+          opacity
+        }
+        this.showAbs = false
+      } else if (top > 140) {
+        this.showAbs = false
+      }else {
+        this.showAbs = true
+      }
+    }
+  },
+  activated () {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+
+.header-abs
+  position absolute
+  left .2rem
+  top .2rem
+  width .8rem
+  height .8rem
+  line-height .8rem
+  text-align center
+  border-radius .4rem
+  background rgba(0, 0, 0, .8)
+  .header-abs-back
+    color #fff
+    font-size .4rem
+.header-fixed
+  position fixed
+  top 0
+  left 0
+  right 0
+  height .86rem
+  line-height .86rem
+  overflow hidden
+  color #fff
+  text-align center
+  background $bgColor
+  .header-fixed-back
+    position absolute
+    top 0
+    left 0
+    width .64rem,
+    font-size .4rem
+    padding-left .2rem
+    color #fff
+</style>
